89b84b1b2562b20956f01bf06c08b81df48ae996
young <ykn9080@empal.com>
2023-07-08 08:45:55 +0900

    init

---
+++ flinkapp/src/main/java/com/fastcampus/yknam/App.java
@@ -0,0 +1,13 @@
+package com.fastcampus.yknam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ flinkapp/src/main/java/com/fastcampus/yknam/WordCount.java
@@ -0,0 +1,7 @@
+package com.fastcampus.yknam;
+
+public class WordCount {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
